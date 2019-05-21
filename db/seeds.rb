2015d5bbd2103379a30d8b162cf6080229a04792
Emeric Le Bail <emeric.lebail@gmail.com>
2019-05-21 17:30:04 +0200

    last push of cocktails day1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint Leaves")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Light Rum")
+Ingredient.create(name: "Dark rum")
+Ingredient.create(name: "Scotch")
+Ingredient.create(name: "Apple Cider")
+Ingredient.create(name: "Apple Juice")
+Ingredient.create(name: "Orange")
